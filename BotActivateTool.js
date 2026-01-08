3130054712fad5ba81db3761df0e4647748d389d
rusk39 <153337087+rusk39@users.noreply.github.com>
2026-01-08 14:49:29 +0900

    css edited

--- BotActivateTool.js
+++ BotActivateTool.js
@@ -4,18 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const cancelButton = document.querySelector('.close')
 
     //使用する関数の宣言
-    function showpopup(str) {
-        alert(str);
-    }
-
-    function cancelAuth(){
-        authModal.style.display = 'none';
-        authButton.style.visibility = 'visible';
-    }
     function startAuth() {
         authModal.style.display = 'block';
         authButton.style.visibility = 'hidden';
     }
+    
+    function cancelAuth(){
+        authModal.style.display = 'none';
+        authButton.style.visibility = 'visible';
+    }
+    
+    
     //ボタン入力に対応した動作を記述
     authButton.addEventListener('click', startAuth);
     cancelButton.addEventListener('click', cancelAuth);
