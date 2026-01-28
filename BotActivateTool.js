05d55201c34e67832cc486c393485e1ef7c7acce
rusk39 <153337087+rusk39@users.noreply.github.com>
2026-01-28 16:32:07 +0900

    many edit

--- BotActivateTool.js
+++ BotActivateTool.js
@@ -1,22 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     //buttonの状態を変数に追加
-    const authButton = document.getElementById('authButton');
-    const cancelButton = document.querySelector('.close')
+    const loginButton = document.getElementById('loginButton');
+    const loginForm = document.getElementById('loginForm');
+    const cancelButton = document.querySelector('.close');
 
     //使用する関数の宣言
-    function startAuth() {
-        authModal.style.display = 'block';
-        authButton.style.visibility = 'hidden';
+    function startLogin(){
+        loginForm.classList.remove('hidden');
+        loginButton.classList.add('hidden');
     }
-    
-    function cancelAuth(){
-        authModal.style.display = 'none';
-        authButton.style.visibility = 'visible';
+    function cancelLogin() {
+        loginForm.classList.add('hidden');
+        loginButton.classList.remove('hidden');
     }
-    
-    
     //ボタン入力に対応した動作を記述
-    authButton.addEventListener('click', startAuth);
-    cancelButton.addEventListener('click', cancelAuth);
+    loginButton.addEventListener('click', startLogin);
+    cancelButton.addEventListener('click', cancelLogin);
 
 });
